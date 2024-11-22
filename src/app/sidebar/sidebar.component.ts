eb4788ce1eaa5a8f8ff40389a83204abd17c58ae
youssef galal <youssefgalal103@gmail.com>
2024-11-22 16:32:53 +0200

    changes in sidebar

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,17 +1,22 @@
 import { Component } from '@angular/core';
+import { MatSidenavContainer, MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [MatSidenavModule, MatIconModule, MatSidenavContainer, FormsModule, MatButtonModule, MatListModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
-export class SidebarComponent {
-isExpanded = true
 
-toggleSidebar(){
-this.isExpanded =!this.isExpanded
-}
+  export class SidebarComponent {
+    isMinimized = false;
 
-}
+    toggleMinimize() {
+      this.isMinimized = !this.isMinimized;
+    }
+  }
