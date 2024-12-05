550ee23e6f9dacbf28da6bca3f3a4a207e876c75
youssefgalal <youssefgalal103@gmail.com>
2024-12-05 19:42:06 +0200

    changes for category

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from "./home/home.component";
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { LoginComponent } from './login/login.component';
+import { MainCategoryComponent } from "./main-category/main-category.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent,SidebarComponent,LoginComponent],
+  imports: [RouterOutlet, HomeComponent, SidebarComponent, LoginComponent, MainCategoryComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/main-category/main-category.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-category',
+  standalone: true,
+  imports: [],
+  templateUrl: './main-category.component.html',
+  styleUrl: './main-category.component.css'
+})
+export class MainCategoryComponent {
+
+}
