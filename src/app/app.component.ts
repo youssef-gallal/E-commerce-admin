81308ecca9afdea52933c68aaa005d9c7a6622f0
youssefgalal <youssefgalal103@gmail.com>
2024-12-07 00:28:23 +0200

    second changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from "./home/home.component";
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { LoginComponent } from './login/login.component';
+import { SubCategoryComponent } from "./sub-category/sub-category.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent, SidebarComponent, LoginComponent],
+  imports: [RouterOutlet, HomeComponent, SidebarComponent, LoginComponent, SubCategoryComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
